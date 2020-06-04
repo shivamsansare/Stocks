510faa68fdae333f7f3b2750926591eb372400e6
Shivam Sansare <shivam.sansare99@gmail.com>
2020-06-04 18:09:11 +0530

    updates

--- app.js
+++ app.js
@@ -88,7 +88,7 @@ app.post("/uploads", upload.single('file'),function(req,res){
             for(i=1;i<result.length-1;i++){
                 var company=result[i].symbol;
                 company=company.toUpperCase();
-                if(company.localeCompare("")){
+                if(!company.localeCompare("")){
                     var newPrice={company:company,date: result[i].date,price: result[i].close};
                     //console.log(newPrice);
                     Prices.create(newPrice,function(err,newEntry){
@@ -123,8 +123,8 @@ app.get("*",function(req,res){
     res.send("Page not available");
 });
 
-const PORT=5000;
+//const PORT=5000;
 
-app.listen(PORT,function(req,res){
+app.listen(process.env.PORT,process.env.IP,function(req,res){
     console.log("hello");
 })
