87b61d3fccaebca7531095641db1006b46492e60
Shivam Sansare <shivam.sansare99@gmail.com>
2020-07-04 23:47:59 +0530

    updates

--- keys.js
+++
@@ -1,5 +0,0 @@
-module.exports={
-    key:{
-        fcsApiKey :"Pg2ROBQHIHhzWxDWxctGF94a3ZQKiy5M5Eqr9f9ePkDQ3kJpaI",
-        mongoDb: "mongodb+srv://root:ait@cluster0-z0ft9.mongodb.net/stock?retryWrites=true&w=majority"}
-};
